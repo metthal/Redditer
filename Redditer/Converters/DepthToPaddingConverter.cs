7d6d93d5298b798c8b192aed9e08a0a6ffae8e8c
Marek Milkovič <milkovic.marek@gmail.com>
2016-10-22 11:29:43 +0200

    Redditer: Added indentation of comments

---
+++ Redditer/Converters/DepthToPaddingConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Redditer.Converters
+{
+    public class DepthToPaddingConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return new Thickness { Left = (int)value * 12 };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
